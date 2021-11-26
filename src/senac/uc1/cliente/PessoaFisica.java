af5d2333300b5600b01989a35e6e142f5bac90d0
LuanRT <luan.lrt4@gmail.com>
2021-11-26 17:26:29 -0300

    format: formatar código

--- src/senac/uc1/cliente/PessoaFisica.java
+++ src/senac/uc1/cliente/PessoaFisica.java
@@ -1,6 +1,8 @@
 package senac.uc1.cliente;
 
 public class PessoaFisica extends Cliente {
+	private String cpf;
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
@@ -8,10 +10,10 @@ public class PessoaFisica extends Cliente {
 	public String getCpf() {
 		return cpf;
 	}
-	
+
 	@Override
-        public String toString(){
-            return super.toString() + "\nPessoaFisica" + "CPF: " + cpf;
-        }
-	
+	public String toString() {
+		return super.toString() + "\n" +
+			"CPF: " + this.cpf;
+	}
 }
