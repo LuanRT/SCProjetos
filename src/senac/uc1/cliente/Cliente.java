281ea2d11f333bdfed49d43bc818737b4e8d4d14
LuanRT <luan.lrt4@gmail.com>
2021-11-26 16:10:29 -0300

    chore: adiciona classe Cliente

--- src/senac/uc1/cliente/Cliente.java
+++ src/senac/uc1/cliente/Cliente.java
@@ -1,1 +1,37 @@
-// TODO: adicionar resto do código
+package senac.uc1.cliente;
+
+public class Cliente {
+	private String nome, endereco, telefone, email;
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+}
