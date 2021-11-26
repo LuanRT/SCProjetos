3b1ea98635d758478298a274709a0df825e62e66
LuanRT <luan.lrt4@gmail.com>
2021-11-26 16:14:10 -0300

    chore: adiciona classes para pessoa física e jurídica

--- src/senac/uc1/cliente/PessoaFisica.java
+++ src/senac/uc1/cliente/PessoaFisica.java
@@ -1,1 +1,13 @@
-// TODO: adicionar resto do código
+package senac.uc1.cliente;
+
+public class PessoaFisica extends Cliente {
+	private String cpf;
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+}
--- src/senac/uc1/cliente/PessoaJuridica.java
+++ src/senac/uc1/cliente/PessoaJuridica.java
@@ -1,1 +1,13 @@
-// TODO: adicionar resto do código
+package senac.uc1.cliente;
+
+public class PessoaJuridica extends Cliente {
+	private String cnpj;
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+}
