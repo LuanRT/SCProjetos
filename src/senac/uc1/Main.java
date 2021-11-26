94d3abdb3814564aa4e66206e0e8bfb5e0f30ef5
LuanRT <luan.lrt4@gmail.com>
2021-11-26 16:16:56 -0300

    chore: adiciona código da classe Main

--- src/senac/uc1/Main.java
+++ src/senac/uc1/Main.java
@@ -1,1 +1,12 @@
-//TODO: Adicionar resto do código aqui
+package senac.uc1;
+
+import java.util.*;
+import senac.uc1.cliente.*;
+
+public class Main {
+	private static List lista_de_clientes;
+	
+	public static void main(String... args) {
+		lista_de_clientes = new ArrayList<Cliente>();
+	}
+}
