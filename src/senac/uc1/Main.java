dd8cbd37839f6a5c5d5455e93c7ec46cafc76cf4
LuanRT <luan.lrt4@gmail.com>
2021-11-28 16:18:58 -0300

    chore: format code

--- src/senac/uc1/Main.java
+++ src/senac/uc1/Main.java
@@ -5,228 +5,233 @@ import senac.uc1.cliente.*;
 import senac.uc1.utils.*;
 
 public class Main {
-  private static int estado_index = 0;
-  private static State estado_atual;
-  private static ArrayList<Cliente> lista_de_clientes;
-  private static Scanner sc;
+    private static int estado_index = 0;
+    private static State estado_atual;
+    private static ArrayList<Cliente> lista_de_clientes;
+    private static Scanner sc;
 
-  public static void main(String...args) {
-    sc = new Scanner(System.in);
-    lista_de_clientes = new ArrayList<Cliente>();
+    public static void main(String...args) {
+        sc = new Scanner(System.in);
+        lista_de_clientes = new ArrayList<Cliente>();
 
-    // Seta o estado inicial do programa para menu principal
-    setSistemaEstado(State.MENU_PRINCIPAL);
-
-    // Inicializa o loop principal do programa
-    init();
-  }
-
-  private static void init() {
-    /* 
-     * Executa as acoes do sistema de acordo com
-     * o estado atual do programa
-     */
-    while (estado_atual != State.PARADO) {
-      switch (estado_atual.toString()) {
-      case "PARANDO":
-        stopSystem();
-        break;
-      case "CADASTRANDO_CLIENT":
-        cadastraClient();
-        break;
-      case "REMOVENDO_CLIENT":
-        removeClient();
-        break;
-      case "MODIFICANDO_CLIENT":
-        modificaClient();
-        break;
-      case "EXIBINDO_STATS":
-        mostraStats();
-        break;
-      case "EXIBINDO_DETALHES":
-        consultaClient();
-        break;
-      case "MENU_PRINCIPAL":
-        mostraMenuPrincipal();
-        break;
-      default:
-        throw new Error(Constants.ESTADO_INVALIDO);
-      }
-    }
-  }
-
-  // Mostra quantidade de clientes registrados
-  private static void mostraStats() {
-    System.out.println();
-    System.out.println(Constants.ESTATISTICAS);
-    System.out.println(Constants.CLIENTES_REGISTRADOS + lista_de_clientes.size());
-    System.out.println();
-
-    delay(1200);
-
-    setSistemaEstado(State.MENU_PRINCIPAL);
-  }
-
-  private static void consultaClient() {
-    // Verifica se a lista tem pelo menos 1 cliente registrado
-    if (lista_de_clientes.size() == 0) {
-      System.out.println();
-      System.out.println(Constants.CLIENTS_INDISPONIVEL);
-      System.out.println();
-      delay(1200);
-      setSistemaEstado(State.MENU_PRINCIPAL);
-      return;
-    }
-
-    // Se tiver então pede o usuario para escrever o email de algum cliente
-    System.out.println();
-    System.out.println(Constants.CONSULTA_CLIENTS);
-    System.out.println();
-
-    System.out.println(Constants.DIGITE_EMAIL_DO_CLIENTE);
-    String email = sc.next();
-    System.out.println();
-
-    System.out.println(Constants.PROCURANDO_CLIENT);
-    delay(1200);
-
-    // Verifica cada cliente da lista ate encontrar o certo
-    for (Cliente client: lista_de_clientes) {
-      if (client.getEmail().equals(email)) {
-        System.out.println();
-        System.out.println(client.toString());
-        System.out.println();
-        delay(1200);
+        // Seta o estado inicial do programa para menu principal
         setSistemaEstado(State.MENU_PRINCIPAL);
-        return;
-      }
+
+        // Inicializa o loop principal do programa
+        init();
     }
 
-    // Caso não seja encontrado nada entao avisa o usuario e volta ao menu principal
-    System.out.println(Constants.CLIENT_NAO_ENCONTRADO.replace("${email}", email));
-    System.out.println();
-
-    setSistemaEstado(State.MENU_PRINCIPAL);
-  }
-
-  private static void cadastraClient() {
-    System.out.println();
-    System.out.println(Constants.CLIENT_REGISTRO);
-    System.out.println(Constants.REGISTRO_PROGRESSO);
-    System.out.println();
-
-    String nome, endereco, numero, email, tipo;
-    Cliente client = new Cliente();
-
-    // Isso é necessario, consome as linhas extras
-    sc.nextLine();
-
-    System.out.println("Nome: ");
-    nome = sc.nextLine();
-    System.out.println();
-
-    System.out.println("Endereço: ");
-    endereco = sc.nextLine();
-    System.out.println();
-
-    System.out.println("Telefone: ");
-    numero = sc.nextLine();
-    System.out.println();
-
-    System.out.println("E-mail: ");
-    email = sc.nextLine();
-    System.out.println();
-
-    System.out.println(Constants.TIPO_DE_CLIENT_MSG);
-    tipo = sc.nextLine();
-    System.out.println();
-
-    // Cria o objeto do cliente de acordo com seu tipo
-    if (tipo.toLowerCase().equals("f")) {
-      System.out.println("CPF: ");
-
-      String cpf = sc.nextLine();
-
-      PessoaFisica pessoa_fisica = new PessoaFisica();
-      pessoa_fisica.setCpf(cpf);
-
-      client = pessoa_fisica;
-    } else if (tipo.toLowerCase().equals("j")) {
-      System.out.println("CNPJ: ");
-
-      String cnpj = sc.nextLine();
-
-      PessoaJuridica pessoa_juridica = new PessoaJuridica();
-      pessoa_juridica.setCnpj(cnpj);
-
-      client = pessoa_juridica;
-    } else {
-      // Recomeca o processo caso o usuario selecione uma opcao invalida
-      System.out.println(Constants.TIPO_INVALIDO_MSG);
-      return;
+    private static void init() {
+        /* 
+         * Executa as acoes do sistema de acordo com
+         * o estado atual do programa
+         */
+        while (estado_atual != State.PARADO) {
+            switch (estado_atual.toString()) {
+                case "PARANDO":
+                    stopSystem();
+                    break;
+                case "CADASTRANDO_CLIENT":
+                    cadastraClient();
+                    break;
+                case "REMOVENDO_CLIENT":
+                    removeClient();
+                    break;
+                case "MODIFICANDO_CLIENT":
+                    modificaClient();
+                    break;
+                case "EXIBINDO_STATS":
+                    mostraStats();
+                    break;
+                case "EXIBINDO_DETALHES":
+                    consultaClient();
+                    break;
+                case "MENU_PRINCIPAL":
+                    mostraMenuPrincipal();
+                    break;
+                default:
+                    throw new Error(Constants.ESTADO_INVALIDO);
+            }
+        }
     }
 
-    // Seta as informacoes restantes & adiciona o cliente a lista
-    client.setNome(nome);
-    client.setEndereco(endereco);
-    client.setTelefone(numero);
-    client.setEmail(email);
+    // Mostra quantidade de clientes registrados
+    private static void mostraStats() {
+        System.out.println();
+        System.out.println(Constants.ESTATISTICAS);
+        System.out.println(Constants.CLIENTES_REGISTRADOS + lista_de_clientes.size());
+        System.out.println();
 
-    lista_de_clientes.add(client);
+        delay(1200);
 
-    System.out.println();
-    System.out.println(Constants.REGISTRO_SUCESSO_MSG);
-    System.out.println();
-
-    delay(1200);
-
-    setSistemaEstado(State.MENU_PRINCIPAL);
-  }
-
-  private static void removeClient() { /* TODO: Implementar isso */ }
-  private static void modificaClient() { /* TODO: Implementar isso */ }
-
-  // Para o sistema completamente
-  private static void stopSystem() {
-    System.out.println();
-    System.out.println(Constants.GOODBYE);
-    setSistemaEstado(State.PARADO);
-  }
-
-  // Printa o menu principal e pede um novo estado para o sistema
-  private static void mostraMenuPrincipal() {
-    System.out.println(Constants.MENU_PRINCIPAL);
-    getNewState();
-  }
-
-  // Pega o novo estado do sistema apos interacao com o usuario
-  private static void getNewState() {
-    estado_index = sc.nextInt();
-
-    if (estado_index > 5) {
-      /*
-       * Somente opcoes abaixo ou iguais a posicao 5 sao validas para o usuario,
-       * pois acima disso sao acoes internas do sistema
-       */
-
-      System.out.println();
-      System.out.println(Constants.OPCAO_INVALIDA);
-      estado_index = 7; // 7 é a posicao de “MENU_PRINCIPAL” no enum
+        setSistemaEstado(State.MENU_PRINCIPAL);
     }
 
-    delay(500);
+    private static void consultaClient() {
+        // Verifica se a lista tem pelo menos 1 cliente registrado
+        if (lista_de_clientes.size() == 0) {
+            System.out.println();
+            System.out.println(Constants.CLIENTS_INDISPONIVEL);
+            System.out.println();
+            delay(1200);
+            setSistemaEstado(State.MENU_PRINCIPAL);
+            return;
+        }
 
-    setSistemaEstado(State.get(estado_index));
-  }
+        // Se tiver então pede o usuario para escrever o email de algum cliente
+        System.out.println();
+        System.out.println(Constants.CONSULTA_CLIENTS);
+        System.out.println();
 
-  private static void setSistemaEstado(State state) {
-    estado_atual = state;
-  }
+        System.out.println(Constants.DIGITE_EMAIL_DO_CLIENTE);
+        String email = sc.next();
+        System.out.println();
 
-  // Faz a thread principal pausar por uma determinada quantidade de tempo, isso ajuda a manter a aplicacao mais dinamica
-  private static void delay(int millis) {
-    try {
-      Thread.sleep(millis);
-    } catch (InterruptedException e) {}
-  }
+        System.out.println(Constants.PROCURANDO_CLIENT);
+        delay(1200);
+
+        // Verifica cada cliente da lista ate encontrar o certo
+        for (Cliente client: lista_de_clientes) {
+            if (client.getEmail().equals(email)) {
+                System.out.println();
+                System.out.println(client.toString());
+                System.out.println();
+                delay(1200);
+                setSistemaEstado(State.MENU_PRINCIPAL);
+                return;
+            }
+        }
+
+        // Caso não seja encontrado nada entao avisa o usuario e volta ao menu principal
+        System.out.println(Constants.CLIENT_NAO_ENCONTRADO.replace("${email}", email));
+        System.out.println();
+
+        setSistemaEstado(State.MENU_PRINCIPAL);
+    }
+
+    private static void cadastraClient() {
+        System.out.println();
+        System.out.println(Constants.CLIENT_REGISTRO);
+        System.out.println(Constants.REGISTRO_PROGRESSO);
+        System.out.println();
+
+        String nome, endereco, numero, email, tipo;
+        Cliente client = new Cliente();
+
+        // Isso é necessario, consome as linhas extras
+        sc.nextLine();
+
+        System.out.println("Nome: ");
+        nome = sc.nextLine();
+        System.out.println();
+
+        System.out.println("Endereço: ");
+        endereco = sc.nextLine();
+        System.out.println();
+
+        System.out.println("Telefone: ");
+        numero = sc.nextLine();
+        System.out.println();
+
+        System.out.println("E-mail: ");
+        email = sc.nextLine();
+        System.out.println();
+
+        System.out.println(Constants.TIPO_DE_CLIENT_MSG);
+        tipo = sc.nextLine();
+        System.out.println();
+
+        // Cria o objeto do cliente de acordo com seu tipo
+        if (tipo.toLowerCase().equals("f")) {
+            System.out.println("CPF: ");
+
+            String cpf = sc.nextLine();
+
+            PessoaFisica pessoa_fisica = new PessoaFisica();
+            pessoa_fisica.setCpf(cpf);
+
+            client = pessoa_fisica;
+        } else if (tipo.toLowerCase().equals("j")) {
+            System.out.println("CNPJ: ");
+
+            String cnpj = sc.nextLine();
+
+            PessoaJuridica pessoa_juridica = new PessoaJuridica();
+            pessoa_juridica.setCnpj(cnpj);
+
+            client = pessoa_juridica;
+        } else {
+            // Recomeca o processo caso o usuario selecione uma opcao invalida
+            System.out.println(Constants.TIPO_INVALIDO_MSG);
+            return;
+        }
+
+        // Seta as informacoes restantes & adiciona o cliente a lista
+        client.setNome(nome);
+        client.setEndereco(endereco);
+        client.setTelefone(numero);
+        client.setEmail(email);
+
+        lista_de_clientes.add(client);
+
+        System.out.println();
+        System.out.println(Constants.REGISTRO_SUCESSO_MSG);
+        System.out.println();
+
+        delay(1200);
+
+        setSistemaEstado(State.MENU_PRINCIPAL);
+    }
+
+    private static void removeClient() {
+        /* TODO: Implementar isso */
+    }
+    private static void modificaClient() {
+        /* TODO: Implementar isso */
+    }
+
+    // Para o sistema completamente
+    private static void stopSystem() {
+        System.out.println();
+        System.out.println(Constants.GOODBYE);
+        setSistemaEstado(State.PARADO);
+    }
+
+    // Printa o menu principal e pede um novo estado para o sistema
+    private static void mostraMenuPrincipal() {
+        System.out.println(Constants.MENU_PRINCIPAL);
+        getNewState();
+    }
+
+    // Pega o novo estado do sistema apos interacao com o usuario
+    private static void getNewState() {
+        estado_index = sc.nextInt();
+
+        if (estado_index > 5) {
+            /*
+             * Somente opcoes abaixo ou iguais a posicao 5 sao validas para o usuario,
+             * pois acima disso sao acoes internas do sistema
+             */
+
+            System.out.println();
+            System.out.println(Constants.OPCAO_INVALIDA);
+            estado_index = 7; // 7 é a posicao de “MENU_PRINCIPAL” no enum
+        }
+
+        delay(500);
+
+        setSistemaEstado(State.get(estado_index));
+    }
+
+    // Seta o estado atual do sistema
+    private static void setSistemaEstado(State state) {
+        estado_atual = state;
+    }
+
+    // Faz a thread principal pausar por uma determinada quantidade de tempo, isso ajuda a manter a aplicacao mais dinamica
+    private static void delay(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {}
+    }
 }
--- src/senac/uc1/utils/Constants.java
+++ src/senac/uc1/utils/Constants.java
@@ -1,63 +1,63 @@
 package senac.uc1.utils;
 
 public class Constants {
-  public static final String
-    // Menu principal & Outros:
-    MENU_PRINCIPAL =
-    "⚙️ -- Gerenciador De Clientes -- ⚙️" + "\n\n" +
-    "O que gostaria de fazer hoje?\nDigite o número da opção desejada:" + "\n\n" +
-    "1. Cadastrar cliente" + "\n" +
-    "2. Remover cliente" + "\n" +
-    "3. Atualizar dados de cliente" + "\n" +
-    "4. Exibir estatísticas" + "\n" +
-    "5. Consultar cliente" + "\n\n" +
-    "Digite 0 para sair do programa.",
+    public static final String
+        // Menu principal & Outros:
+        MENU_PRINCIPAL =
+        "⚙️ -- Gerenciador De Clientes -- ⚙️" + "\n\n" +
+        "O que gostaria de fazer hoje?\nDigite o número da opção desejada:" + "\n\n" +
+        "1. Cadastrar cliente" + "\n" +
+        "2. Remover cliente" + "\n" +
+        "3. Atualizar dados de cliente" + "\n" +
+        "4. Exibir estatísticas" + "\n" +
+        "5. Consultar cliente" + "\n\n" +
+        "Digite 0 para sair do programa.",
 
-    OPCAO_INVALIDA =
-    "❌ Opção inválida! Escolha somente números\nque estejam presentes no menu:\n",
+        OPCAO_INVALIDA =
+        "❌ Opção inválida! Escolha somente números\nque estejam presentes no menu:\n",
 
-    GOODBYE =
-    "👋 Até mais!",
+        GOODBYE =
+        "👋 Até mais!",
 
-    // Mensagens de erro:
-    ESTADO_INVALIDO =
-    "[SYS-ERROR]: O sistema tentou acessar um estado não existente!",
+        // Mensagens de erro:
+        ESTADO_INVALIDO =
+        "[SYS-ERROR]: O sistema tentou acessar um estado não existente!",
 
-    // Tela de registro de clientes: 
-    CLIENT_REGISTRO =
-    "🗃️ Cadastro De Cliente 🗃️",
+        // Tela de registro de clientes: 
+        CLIENT_REGISTRO =
+        "🗃️ Cadastro De Cliente 🗃️",
 
-    REGISTRO_PROGRESSO =
-    "Preencha os dados cadastrais de seu cliente:",
+        REGISTRO_PROGRESSO =
+        "Preencha os dados cadastrais de seu cliente:",
 
-    TIPO_DE_CLIENT_MSG =
-    "Certo!\nAgora me diga em qual categoria esse cliente se encaixa:\npessoa fisica ou juridica (f/j)?",
+        TIPO_DE_CLIENT_MSG =
+        "Certo!\nAgora me diga em qual categoria esse cliente se encaixa:\npessoa fisica ou juridica (f/j)?",
 
-    REGISTRO_SUCESSO_MSG =
-    "✅ Usuário cadastrado com sucesso!",
+        REGISTRO_SUCESSO_MSG =
+        "✅ Usuário cadastrado com sucesso!",
 
-    TIPO_INVALIDO_MSG =
-    "❌ Opção inválida",
+        TIPO_INVALIDO_MSG =
+        "❌ Opção inválida",
 
-    // Estatisticas & Detalhes de clientes
-    ESTATISTICAS =
-    "📊 Estatísticas 📊",
+        // Estatisticas & Detalhes de clientes
+        ESTATISTICAS =
+        "📊 Estatísticas 📊",
 
-    CLIENTES_REGISTRADOS =
-    "Clientes cadastrados: ",
+        CLIENTES_REGISTRADOS =
+        "Clientes cadastrados: ",
 
-    CONSULTA_CLIENTS =
-    "📋 Consulta De Cliente 📋",
+        CONSULTA_CLIENTS =
+        "📋 Consulta De Cliente 📋",
 
-    CLIENTS_INDISPONIVEL =
-    "Nenhum cliente registrado no sistema!",
+        CLIENTS_INDISPONIVEL =
+        "Nenhum cliente registrado no sistema!",
 
-    CLIENT_NAO_ENCONTRADO =
-    "Não foi encontrado nenhum cliente com e-mail ${email}!",
+        CLIENT_NAO_ENCONTRADO =
+        "Não foi encontrado nenhum cliente com e-mail ${email}!",
 
-    DIGITE_EMAIL_DO_CLIENTE =
-    "Digite o endereço de e-mail do cliente no qual deseja consultar\nE-mail:",
+        DIGITE_EMAIL_DO_CLIENTE =
+        "Digite o endereço de e-mail do cliente no qual deseja consultar\nE-mail:",
 
-    PROCURANDO_CLIENT =
-    "🔍 Procurando...";
+        PROCURANDO_CLIENT =
+        "🔍 Procurando...";
 }
