b6bdc0679de74c4f115b9e4eb7117f9b3c0beea5
LuanRT <luan.lrt4@gmail.com>
2021-11-29 07:09:10 -0300

    feat: add suporte a remoção & atualização de cliente

--- src/senac/uc1/Main.java
+++ src/senac/uc1/Main.java
@@ -1,11 +1,10 @@
 package senac.uc1;
 
 import java.util.*;
-import senac.uc1.cliente.*;
 import senac.uc1.utils.*;
+import senac.uc1.cliente.*;
 
 public class Main {
-    private static int estado_index = 0;
     private static State estado_atual;
     private static ArrayList<Cliente> lista_de_clientes;
     private static Scanner sc;
@@ -67,46 +66,9 @@ public class Main {
         setSistemaEstado(State.MENU_PRINCIPAL);
     }
 
-    private static void consultaClient() {
-        // Verifica se a lista tem pelo menos 1 cliente registrado
-        if (lista_de_clientes.size() == 0) {
-            System.out.println();
-            System.out.println(Constants.CLIENTS_INDISPONIVEL);
-            System.out.println();
-            delay(1200);
-            setSistemaEstado(State.MENU_PRINCIPAL);
-            return;
-        }
-
-        // Se tiver então pede o usuario para escrever o email de algum cliente
-        System.out.println();
-        System.out.println(Constants.CONSULTA_CLIENTS);
-        System.out.println();
-
-        System.out.println(Constants.DIGITE_EMAIL_DO_CLIENTE);
-        String email = sc.next();
-        System.out.println();
-
-        System.out.println(Constants.PROCURANDO_CLIENT);
-        delay(1200);
-
-        // Verifica cada cliente da lista ate encontrar o certo
-        for (Cliente client: lista_de_clientes) {
-            if (client.getEmail().equals(email)) {
-                System.out.println();
-                System.out.println(client.toString());
-                System.out.println();
-                delay(1200);
-                setSistemaEstado(State.MENU_PRINCIPAL);
-                return;
-            }
-        }
-
-        // Caso não seja encontrado nada entao avisa o usuario e volta ao menu principal
-        System.out.println(Constants.CLIENT_NAO_ENCONTRADO.replace("${email}", email));
-        System.out.println();
-
-        setSistemaEstado(State.MENU_PRINCIPAL);
+    private static void mostraMenuPrincipal() {
+        System.out.println(Constants.MENU_PRINCIPAL);
+        getNewState();
     }
 
     private static void cadastraClient() {
@@ -162,7 +124,7 @@ public class Main {
             client = pessoa_juridica;
         } else {
             // Recomeca o processo caso o usuario selecione uma opcao invalida
-            System.out.println(Constants.TIPO_INVALIDO_MSG);
+            System.out.println(Constants.OP_INVALIDO_MSG);
             return;
         }
 
@@ -183,29 +145,215 @@ public class Main {
         setSistemaEstado(State.MENU_PRINCIPAL);
     }
 
-    private static void removeClient() {
-        /* TODO: Implementar isso */
-    }
-    private static void modificaClient() {
-        /* TODO: Implementar isso */
-    }
+    private static void consultaClient() {
+        // Verifica se a lista tem pelo menos 1 cliente registrado
+        if (lista_de_clientes.size() == 0) {
+            System.out.println();
+            System.out.println(Constants.CLIENTS_INDISPONIVEL);
+            System.out.println();
 
-    // Para o sistema completamente
-    private static void stopSystem() {
+            delay(1200);
+
+            setSistemaEstado(State.MENU_PRINCIPAL);
+            return;
+        }
+
+        // Se tiver então pede o usuario para escrever o email de algum cliente
         System.out.println();
-        System.out.println(Constants.GOODBYE);
-        setSistemaEstado(State.PARADO);
+        System.out.println(Constants.CONSULTA_CLIENTS);
+        System.out.println();
+
+        System.out.println(Constants.DIGITE_EMAIL_DO_CLIENTE_2);
+        String email = sc.next();
+        System.out.println();
+
+        System.out.println(Constants.PROCURANDO_CLIENT);
+        delay(1200);
+
+        // Procura na lista algum cliente que tenha o email fornecido
+        Cliente client = findClient(email);
+        if (client != null) {
+            System.out.println();
+            System.out.println(Constants.CLIENT_ENCONTRADO);
+            System.out.println(client.toString());
+            System.out.println();
+            delay(1200);
+            setSistemaEstado(State.MENU_PRINCIPAL);
+        } else {
+            System.out.println(Constants.CLIENT_NAO_ENCONTRADO.replace("${email}", email));
+            System.out.println();
+
+            setSistemaEstado(State.MENU_PRINCIPAL);
+        }
     }
 
-    // Printa o menu principal e pede um novo estado para o sistema
-    private static void mostraMenuPrincipal() {
-        System.out.println(Constants.MENU_PRINCIPAL);
-        getNewState();
+    private static void removeClient() {
+        // Verifica se a lista tem pelo menos 1 cliente registrado
+        if (lista_de_clientes.size() == 0) {
+            System.out.println();
+            System.out.println(Constants.CLIENTS_INDISPONIVEL);
+            System.out.println();
+
+            delay(1200);
+
+            setSistemaEstado(State.MENU_PRINCIPAL);
+            return;
+        }
+
+        System.out.println();
+        System.out.println(Constants.REMOVE_CLIENT);
+
+        // Consome linha extra
+        sc.nextLine();
+
+        System.out.println(Constants.DIGITE_EMAIL_DO_CLIENTE_1);
+        String email = sc.nextLine();
+        System.out.println();
+
+        System.out.println(Constants.REMOVENDO_CLIENT);
+        delay(1200);
+
+        // Procura na lista algum cliente que tenha o mesmo email
+        Cliente client = findClient(email);
+        if (client != null) {
+            lista_de_clientes.remove(client);
+            System.out.println(Constants.CLIENT_REMOVIDO);
+            System.out.println();
+            delay(1200);
+            setSistemaEstado(State.MENU_PRINCIPAL);
+        } else {
+            System.out.println(Constants.CLIENT_NAO_ENCONTRADO.replace("${email}", email));
+            System.out.println();
+
+            setSistemaEstado(State.MENU_PRINCIPAL);
+        }
+    }
+
+    private static void modificaClient() {
+        // Verifica se a lista tem pelo menos 1 cliente registrado
+        if (lista_de_clientes.size() == 0) {
+            System.out.println();
+            System.out.println(Constants.CLIENTS_INDISPONIVEL);
+            System.out.println();
+
+            delay(1200);
+
+            setSistemaEstado(State.MENU_PRINCIPAL);
+            return;
+        }
+
+        System.out.println();
+        System.out.println(Constants.UPDATE_INFO);
+
+        // Consome linha extra
+        sc.nextLine();
+
+        System.out.println(Constants.DIGITE_EMAIL_DO_CLIENTE_0);
+        String email = sc.nextLine();
+        System.out.println();
+
+        System.out.println(Constants.PROCURANDO_CLIENT);
+        delay(1200);
+
+        // Procura na lista algum cliente que tenha o mesmo email
+        Cliente client = findClient(email);
+        if (client != null) {
+            System.out.println();
+            System.out.println(Constants.CLIENT_ENCONTRADO);
+            System.out.println(client.toString());
+
+            System.out.println();
+            System.out.println(Constants.UPDATE_INFO_OPCOES);
+            System.out.println();
+
+            int opcao = sc.nextInt();
+            switch (opcao) {
+                case 1: // Nome
+                    sc.nextLine();
+
+                    System.out.println("Novo nome:");
+                    String nome = sc.nextLine();
+                    System.out.println();
+
+                    client.setNome(nome);
+
+                    System.out.println(Constants.UPDATE_NOME_SUCESSO_MSG);
+                    System.out.println();
+                    break;
+                case 2: // Endereco
+                    sc.nextLine();
+
+                    System.out.println("Novo endereço:");
+                    String endereco = sc.nextLine();
+                    System.out.println();
+
+                    client.setEndereco(endereco);
+
+                    System.out.println(Constants.UPDATE_ENDERECO_SUCESSO_MSG);
+                    System.out.println();
+                    break;
+                case 3: // Telefone
+                    sc.nextLine();
+
+                    System.out.println("Novo telefone:");
+                    String telefone = sc.nextLine();
+                    System.out.println();
+
+                    client.setTelefone(telefone);
+
+                    System.out.println(Constants.UPDATE_TELEFONE_SUCESSO_MSG);
+                    System.out.println();
+                    break;
+                case 4: // E-mail
+                    sc.nextLine();
+
+                    System.out.println("Novo e-mail:");
+                    String new_email = sc.nextLine();
+                    System.out.println();
+
+                    client.setEmail(new_email);
+
+                    System.out.println(Constants.UPDATE_EMAIL_SUCESSO_MSG);
+                    System.out.println();
+                    break;
+                case 5: // CPF ou CNPJ
+                    sc.nextLine();
+
+                    System.out.println("Novo CPF/CNPJ:");
+                    String doc = sc.nextLine();
+                    System.out.println();
+
+                    // Identifica o tipo de cliente e procede
+                    if (client.toString().contains("CNPJ:")) {
+                        PessoaJuridica pessoa_juridica = (PessoaJuridica) client;
+                        pessoa_juridica.setCnpj(doc);
+                    } else {
+                        PessoaFisica pessoa_fisica = (PessoaFisica) client;
+                        pessoa_fisica.setCpf(doc);
+                    }
+
+                    System.out.println(Constants.UPDATE_DOC_SUCESSO_MSG);
+                    System.out.println();
+                    break;
+                default:
+                    System.out.println(Constants.OP_INVALIDO_MSG);
+                    System.out.println();
+                    break;
+            }
+
+            delay(1200);
+            setSistemaEstado(State.MENU_PRINCIPAL);
+        } else {
+            System.out.println(Constants.CLIENT_NAO_ENCONTRADO.replace("${email}", email));
+            System.out.println();
+
+            setSistemaEstado(State.MENU_PRINCIPAL);
+        }
     }
 
     // Pega o novo estado do sistema apos interacao com o usuario
     private static void getNewState() {
-        estado_index = sc.nextInt();
+        int estado_index = sc.nextInt();
 
         if (estado_index > 5) {
             /*
@@ -223,12 +371,29 @@ public class Main {
         setSistemaEstado(State.get(estado_index));
     }
 
+    // Verifica cada cliente da lista ate encontrar o que contenha o email dado
+    private static Cliente findClient(String email) {
+        for (Cliente client: lista_de_clientes) {
+            if (client.getEmail().equals(email)) {
+                return client;
+            }
+        }
+        return null;
+    }
+
+    // Para o sistema completamente
+    private static void stopSystem() {
+        System.out.println();
+        System.out.println(Constants.GOODBYE);
+        setSistemaEstado(State.PARADO);
+    }
+
     // Seta o estado atual do sistema
     private static void setSistemaEstado(State state) {
         estado_atual = state;
     }
 
-    // Faz a thread principal pausar por uma determinada quantidade de tempo, isso ajuda a manter a aplicacao mais dinamica
+    // Faz a thread principal pausar por uma determinada quantidade de tempo (em milissegundos), isso ajuda a manter a aplicacao mais dinamica
     private static void delay(int millis) {
         try {
             Thread.sleep(millis);
--- src/senac/uc1/utils/Constants.java
+++ src/senac/uc1/utils/Constants.java
@@ -36,10 +36,53 @@ public class Constants {
         REGISTRO_SUCESSO_MSG =
         "✅ Usuário cadastrado com sucesso!",
 
-        TIPO_INVALIDO_MSG =
+        OP_INVALIDO_MSG =
         "❌ Opção inválida",
 
-        // Estatisticas & Detalhes de clientes
+        // Tela de atualizacao de clientes:
+        UPDATE_INFO =
+        "📋 Atualizar Informações 📋",
+
+        UPDATE_INFO_OPCOES =
+        "Digite o número do campo no qual deseja atualizar:" + "\n" +
+        "1. Nome" + "\n" +
+        "2. Endereço" + "\n" +
+        "3. Telefone" + "\n" +
+        "4. E-mail" + "\n" +
+        "5. CPF/CNPJ",
+
+        DIGITE_EMAIL_DO_CLIENTE_0 =
+        "Digite o endereço de e-mail do cliente no qual deseja atualizar\nE-mail:",
+
+        UPDATE_NOME_SUCESSO_MSG =
+        "✅ Nome atualizado com sucesso!",
+
+        UPDATE_ENDERECO_SUCESSO_MSG =
+        "✅ Endereço atualizado com sucesso!",
+
+        UPDATE_TELEFONE_SUCESSO_MSG =
+        "✅ Telefone atualizado com sucesso!",
+
+        UPDATE_EMAIL_SUCESSO_MSG =
+        "✅ Endereço de e-mail atualizado com sucesso!",
+
+        UPDATE_DOC_SUCESSO_MSG =
+        "✅ Documentos atualizados com sucesso!",
+
+        // Tela de remocao de clientes:
+        REMOVE_CLIENT =
+        "🗄️ Descadastrar Cliente 🗄️",
+
+        REMOVENDO_CLIENT =
+        "Removendo cliente...",
+
+        DIGITE_EMAIL_DO_CLIENTE_1 =
+        "Digite o endereço de e-mail do cliente no qual deseja descadastrar\nE-mail:",
+
+        CLIENT_REMOVIDO =
+        "✅ Cliente removido do sistema com sucesso!",
+
+        // Estatisticas & Detalhes de clientes:
         ESTATISTICAS =
         "📊 Estatísticas 📊",
 
@@ -55,7 +98,10 @@ public class Constants {
         CLIENT_NAO_ENCONTRADO =
         "Não foi encontrado nenhum cliente com e-mail ${email}!",
 
-        DIGITE_EMAIL_DO_CLIENTE =
+        CLIENT_ENCONTRADO =
+        "Cliente encontrado:",
+
+        DIGITE_EMAIL_DO_CLIENTE_2 =
         "Digite o endereço de e-mail do cliente no qual deseja consultar\nE-mail:",
 
         PROCURANDO_CLIENT =
